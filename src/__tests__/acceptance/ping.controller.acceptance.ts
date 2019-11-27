3a977d98b886e96c40f2732ef47d1a094ed95238
Paul Joey Clark <joeytwiddle@gmail.com>
2019-11-27 14:34:33 +0800

    add test for authentication with /users/login

--- src/__tests__/acceptance/ping.controller.acceptance.ts
+++ src/__tests__/acceptance/ping.controller.acceptance.ts
@@ -18,4 +18,14 @@ describe('PingController', () => {
     const res = await client.get('/ping?msg=world').expect(200);
     expect(res.body).to.containEql({greeting: 'Hello from LoopBack'});
   });
+
+  it('can log in with POST /users/login', async () => {
+    const plainAuth = 'testuser:password123';
+    const encodedAuth = Buffer.from(plainAuth).toString('base64');
+    const res = await client
+      .post('/users/login')
+      .set('Authorization', 'Basic ' + encodedAuth)
+      .expect(200);
+    expect(res.body).to.containEql({token: 'example_token_999'});
+  });
 });
