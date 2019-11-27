26ab7ec287b54eb60348d4662a0461e80cfb1737
Paul Joey Clark <joeytwiddle@gmail.com>
2019-11-27 14:19:52 +0800

    add /users/login endpoint, using Basic authentication

--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -1,5 +1,7 @@
-import {Request, RestBindings, get, ResponseObject} from '@loopback/rest';
+import {Request, RestBindings, get, ResponseObject, post} from '@loopback/rest';
 import {inject} from '@loopback/context';
+import {authenticate, AuthenticationBindings} from '@loopback/authentication';
+import {UserProfile} from '@loopback/security';
 
 /**
  * OpenAPI response for ping()
@@ -31,7 +33,11 @@ const PING_RESPONSE: ResponseObject = {
  * A simple controller to bounce back http requests
  */
 export class PingController {
-  constructor(@inject(RestBindings.Http.REQUEST) private req: Request) {}
+  constructor(
+    @inject(RestBindings.Http.REQUEST) private req: Request,
+    @inject(AuthenticationBindings.CURRENT_USER, {optional: true})
+    private user: UserProfile,
+  ) {}
 
   // Map to `GET /ping`
   @get('/ping', {
@@ -47,5 +53,18 @@ export class PingController {
       url: this.req.url,
       headers: Object.assign({}, this.req.headers),
     };
+  }
+
+  @authenticate('BasicStrategy')
+  @post('/users/login')
+  async login(): Promise<{token: string}> {
+    // We expect the Basic strategy will have authenticated a user
+    if (this.user) {
+      // Generate a unique bearer token or this user and store it in the DB
+      const token = 'example_token_999';
+      return {token};
+    } else {
+      throw new Error('User was not authenticated');
+    }
   }
 }
